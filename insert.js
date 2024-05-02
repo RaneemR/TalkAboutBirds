2fac6492b107f88e8119301a0ff56d3cfbdf5064
RaneemR <rramadan@unm.edu>
2024-05-01 18:43:46 -0600

    second

--- insert.js
+++
@@ -1,8 +0,0 @@
-const sqlite3 = require('sqlite3').verbose()
-let db = new sqlite3.Database('./db/user.db');
-let sql = 'INSERT INTO users (first,last,email,password) VALUES ("Laurence","Svekis","example@gmail.com","pass")';
-db.run(sql,[],function(err){
-if(err) {return console.log(err.message) }
-    console.log(`Row ready ${this.lastID}`);
-}) 
-db.close();
